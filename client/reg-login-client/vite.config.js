2cde1ebfc46f6b2fe45ac28d25caede330f89758
Timothy Lim <timothyytl@gmail.com>
2023-09-25 12:45:39 +0800

    updated vite.config

--- client/reg-login-client/vite.config.js
+++ client/reg-login-client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react-bootstrap': 'react-bootstrap/dist/react-bootstrap.esm.js',
+    },
+  }
 })
